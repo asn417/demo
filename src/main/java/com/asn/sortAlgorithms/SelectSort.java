d4697d2309c548d4767932948dc14dd8396707a7
asn417 <895409039@qq.com>
2020-09-22 13:26:56 +0800

    排序算法

---
+++ src/main/java/com/asn/sortAlgorithms/SelectSort.java
@@ -0,0 +1,32 @@
+package com.asn.sortAlgorithms;
+
+/**
+ * @Author: wangsen
+ * @Date: 2020/9/22 11:59
+ * @Description: 选择排序：不稳定
+ **/
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] arr = {5,1,3,6,2,7,4,6,9};
+        selectSort(arr);
+        for (int i:arr) {
+            System.out.println(i);
+        }
+    }
+    public static void selectSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int max = arr[0];
+            int index = 0;
+            for (int j = 0; j < arr.length - i; j++) {
+                if (max < arr[j]){
+                    max = arr[j];
+                    index = j;
+                }
+            }
+            if (arr[arr.length - i - 1] < max){
+                arr[index] = arr[arr.length - i - 1];
+                arr[arr.length -i - 1] = max;
+            }
+        }
+    }
+}
---
+++ src/main/java/com/asn/sortAlgorithms/ShellSort.java
@@ -0,0 +1,18 @@
+package com.asn.sortAlgorithms;
+
+/**
+ * @Author: wangsen
+ * @Date: 2020/9/22 12:24
+ * @Description: 希尔排序：又叫缩小增量排序
+ **/
+public class ShellSort {
+    public static void main(String[] args) {
+
+    }
+    public static void shellSort(int[] arr){
+        int gap = arr.length / 2;
+        for (int i = 0; i < arr.length; i += gap) {
+
+        }
+    }
+}
