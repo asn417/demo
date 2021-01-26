2515f7bf8543dfe0341d19eb212ebcaef1186636
sen_wang <895409039@qq.com>
2021-01-26 18:22:51 +0800

    HJ37

---
+++ src/main/java/com/asn/leetcode/huawei/simple/HJ37.java
@@ -0,0 +1,25 @@
+package com.asn.leetcode.huawei.simple;
+
+/**
+ * @Author: wangsen
+ * @Date: 2021/1/26 18:11
+ * @Description: 统计每个月的兔子数
+ * 有一只兔子，从出生后第3个月起每个月都生一只兔子，小兔子长到第三个月后每个月又生一只兔子，假如兔子都不死，问每个月的兔子总数为多少？
+ * <p>
+ * 本题有多组数据。
+ * <p>
+ * 输入描述:
+ * 输入int型表示month
+ * <p>
+ * 输出描述:
+ * 输出兔子总数int型
+ **/
+public class HJ37 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int getNum(int month) {
+
+    }
+}
