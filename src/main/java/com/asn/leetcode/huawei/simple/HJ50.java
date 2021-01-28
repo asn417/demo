451e50f017afd6783cecf486cc0745db0bcf62fd
sen_wang <895409039@qq.com>
2021-01-28 13:15:06 +0800

    HJ50 四则运算

---
+++ src/main/java/com/asn/leetcode/huawei/simple/HJ50.java
@@ -0,0 +1,30 @@
+package com.asn.leetcode.huawei.simple;
+
+/**
+ * @Author: wangsen
+ * @Date: 2021/1/27 12:40
+ * @Description: 四则运算
+ * 输入一个表达式（用字符串表示），求这个表达式的值。
+ * 保证字符串中的有效字符包括[‘0’-‘9’],‘+’,‘-’, ‘*’,‘/’ ,‘(’， ‘)’,‘[’, ‘]’,‘{’ ,‘}’。且表达式一定合法。
+ * <p>
+ * 输入描述:
+ * 输入一个算术表达式
+ * <p>
+ * 输出描述:
+ * 得到计算结果
+ * <p>
+ * 示例：
+ * 输入：3+2*{1+2*[-4/(8-6)+7]}
+ * 输出：25
+ **/
+public class HJ50 {
+    public static void main(String[] args) {
+        //3+2*{1+2*[-4/(8-6)+7]}=25
+
+    }
+
+    public static String calculate(String expression) {
+
+        return "";
+    }
+}
