d0607516c5c622b670ebc14515ca1318a28472d3
sen_wang <895409039@qq.com>
2021-03-02 10:27:25 +0800

    力扣：下一个更大的数（单调栈）

--- src/main/java/com/asn/leetcode/simple/NextGreaterElement496.java
+++ src/main/java/com/asn/leetcode/simple/NextGreaterElement496.java
@@ -8,19 +8,33 @@ import java.util.Stack;
  */
 public class NextGreaterElement496 {
     public static void main(String[] args) {
-
+        int[] nums1 = {1, 3, 5, 2, 4};
+        int[] nums2 = {6, 5, 4, 3, 2, 1, 7};
+        int[] res = nextGreaterElement(nums1, nums2);
+        for (int num : res) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
     public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> stack = new Stack <> ();
-        HashMap<Integer, Integer> map = new HashMap <> ();
-        int[] res = new int[nums1.length];
-        for (int i = 0; i < nums2.length; i++) {
-            while (!stack.empty() && nums2[i] > stack.peek())
-                map.put(stack.pop(), nums2[i]);
-            stack.push(nums2[i]);
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //遍历nums2
+        for (int num : nums2) {
+            while (!stack.isEmpty()) {
+                Integer peek = stack.peek();
+                if (num > peek) {
+                    map.put(stack.pop(), num);
+                } else {
+                    break;
+                }
+            }
+            stack.push(num);
         }
-        while (!stack.empty())
+        while (!stack.isEmpty()) {
             map.put(stack.pop(), -1);
+        }
+        int[] res = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
             res[i] = map.get(nums1[i]);
         }
