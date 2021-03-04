9a481daff403968f8ea0ae7a8d045361a1d7f3a9
wangsen <895409039@qq.com>
2021-03-04 12:11:24 +0800

    力扣678，判断有效的括号字符串

--- src/main/java/com/asn/leetcode/middle/CheckValidString678.java
+++ src/main/java/com/asn/leetcode/middle/CheckValidString678.java
@@ -1,10 +1,45 @@
 package com.asn.leetcode.middle;
 
+import java.util.Stack;
+
 /**
  * 有效的括号字符串
  */
 public class CheckValidString678 {
     public static void main(String[] args) {
+        String s = "((((()(()()()*()(((((*)()*(**(())))))(())()())(((())())())))))))(((((())*)))()))(()((*()*(*)))(*)()";
+        System.out.println(checkValidString(s));
+    }
 
+    //双栈
+    public static boolean checkValidString(String s) {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            char c = arr[i];
+            if (c == '(') {
+                stack1.push(i);
+            } else if (c == '*') {
+                stack2.push(i);
+            } else {
+                if (!stack1.isEmpty()) {
+                    stack1.pop();
+                } else if (!stack2.isEmpty()) {
+                    stack2.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (stack1.size() > stack2.size()) {
+            return false;
+        }
+        while (!stack1.isEmpty() && !stack2.isEmpty()) {
+            if (stack1.pop() > stack2.pop()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
